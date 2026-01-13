b6a20192459ac75bfbe415e4493622aa591b8d6a
Filip Matura <matura.filip96@gmail.com>
2026-01-13 04:42:22 +0100

    asteroid guided project started

---
+++ Projects/guided_projects/Asteroid_game/README.md
@@ -0,0 +1,11 @@
+
+
+Goals:
+
+- Multi-file python project introduction
+
+- Get used to referencing documentation
+
+- use case of OOP
+
+- 
---
+++ Projects/guided_projects/Asteroid_game/asteroid_game/main.py
@@ -0,0 +1,26 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+        d_time = clock.tick(60)
+        dt = d_time / 1000
+
+
+
+
+if __name__ == "__main__":
+    main()
